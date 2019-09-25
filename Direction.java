0708fa770537c0af1831b3eb1e521ec7698468f3
Chanwoo Choi <39608427+workChanson@users.noreply.github.com>
2019-09-25 16:52:34 +0100

    Update Direction.java

--- Direction.java
+++ Direction.java
@@ -5,22 +5,34 @@ public class Direction {
 	public static Direction direction;
 	public static double degree;
 	
-	public final static Direction N = new Direction();
-	public final static Direction NNE = new Direction();
-	public final static Direction NE = new Direction(); 
-	public final static Direction ENE = new Direction();
 	public final static Direction E = new Direction();
-	public final static Direction ESE = new Direction();
-	public final static Direction SE = new Direction();
-	public final static Direction SSE = new Direction();
-	public final static Direction S = new Direction();
-	public final static Direction SSW = new Direction();
-	public final static Direction SW = new Direction();
-	public final static Direction WSW = new Direction();
-	public final static Direction W = new Direction();
-	public final static Direction WNW = new Direction();
-	public final static Direction NW = new Direction();
+	public final static Direction ENE = new Direction();
+	public final static Direction NE = new Direction(); 
+	public final static Direction NNE = new Direction();
+	public final static Direction N = new Direction();
 	public final static Direction NNW = new Direction();
+	public final static Direction NW = new Direction();
+	public final static Direction WNW = new Direction();
+	public final static Direction W = new Direction();
+	public final static Direction WSW = new Direction();
+	public final static Direction SW = new Direction();
+	public final static Direction SSW = new Direction();
+	public final static Direction S = new Direction();
+	public final static Direction SSE = new Direction();
+	public final static Direction SE = new Direction();
+	public final static Direction ESE = new Direction();
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 
 }
