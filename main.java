837f2be8eabfd686aff520cff7cd0dbe0872fd4b
Chanwoo Choi <39608427+workChanson@users.noreply.github.com>
2019-09-25 23:12:21 +0100

    Update main.java

--- main.java
+++ main.java
@@ -1,15 +1,24 @@
 package uk.ac.ed.inf.powergrab;
 
+import static org.junit.Assert.assertTrue;
+
 import javax.annotation.Generated;
-//for testing purposes
 
 public class main{
 	
 	public static void main(String args[]) {
-		Position p = new Position(55.944425, -3.188396);
-		System.out.println(p.latitude);
-		System.out.println(p.longitude);
-		System.out.println(p.getDegree(Direction.S));
+
+		final Position p0 = new Position(55.944425, -3.188396);
+		Position p1 = p0.nextPosition(Direction.SSE);
+		System.out.println(p1.latitude);
+		System.out.println(p1.longitude);
+		
+		Position p2 = p1.nextPosition(Direction.S);
+		System.out.println(p2.latitude);
+		System.out.println(p2.longitude);
+		
+		Position stop = new Position(55.94384783614024,-3.1882811949702905);
+
 
 	}
 }
